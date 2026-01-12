4c7cb0d791b610c097cbb6d2644ef3ceff999991
Arlecchino77 <arlecchino77@mail.ru>
2026-01-12 17:36:17 +0300

    Шаг 5
    скелет меню

---
+++ FullOven/FullOven.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullOven.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FullOven 1.0");
+            Console.WriteLine("----------------");
+            Console.WriteLine("1) Добавить задачу (пока заглушка)");
+            Console.WriteLine("2) Показать список задач (пока заглушка)");
+            Console.WriteLine("3) Изменить статус задачи (пока заглушка)");
+            Console.WriteLine("4) Удалить задачу (пока заглушка)");
+            Console.WriteLine("0) Выход");
+            Console.WriteLine("----------------");
+            Console.Write("Выберите пункт меню: ");
+
+            var input = Console.ReadLine();
+            Console.WriteLine($"Вы выбрали: {input}");
+            Console.WriteLine("Пока это только каркас. Логика появится в следующих ЛР.");
+
+        }
+    }
+}
